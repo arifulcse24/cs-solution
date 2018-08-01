08419cc3645ea1ef62b94fd1812e6ccff5f1fa89
arifulcse24 <41609769+arifulcse24@users.noreply.github.com>
2018-08-01 22:15:47 +0600

    Add files via upload

---
+++ Gcc.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a,int b)
+{
+    if(a%b==0)
+        return b;
+        else
+        return gcd(b,(a%b));
+}
+
+int main()
+{
+    int n,a,b;
+    cin>>a>>b;
+    n = gcd(a,b);
+    cout<<n<<endl;
+    return 0;
+
+}
